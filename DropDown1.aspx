06c96fde615213093087ec3ca521d7f4ee760812
Siri.G <gangarapu.shirisha@zensar.com>
2021-02-08 09:47:30 +0530

    Add solution file

---
+++ DropDown1.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DropDown1.aspx.cs" Inherits="DropDown.DropDown1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <p>
+		&nbsp;</p>
+	<form id="form1" runat="server">
+	<p style="margin-left: 80px">
+		&nbsp;</p>
+		<p style="margin-left: 160px">
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		DROPDOWN LIST FOR COUNTRY, STATE AND CITY</p>
+	<p style="margin-left: 240px">
+		&nbsp;</p>
+		<p style="margin-left: 240px">
+			SELECT COUNTRY&nbsp;&nbsp;&nbsp;&nbsp; :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<asp:DropDownList ID="ddCountry" runat="server" OnSelectedIndexChanged="ddCountry_SelectedIndexChanged" style="margin-bottom: 0px" AutoPostBack="True" DataTextField="cname" DataValueField="cid">
+			</asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			</p>
+		<p style="margin-left: 240px">
+			&nbsp;</p>
+		<p style="margin-left: 240px">
+			SELECT STATE&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<asp:DropDownList ID="DDState" runat="server" OnSelectedIndexChanged="DDState_SelectedIndexChanged" style="margin-bottom: 0px" AutoPostBack="True" DataTextField="sname" DataValueField="sid">
+			</asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			</p>
+		<p style="margin-left: 240px">
+			&nbsp;</p>
+		<p style="margin-left: 240px">
+			SELECT CITY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<asp:DropDownList ID="DDCity" runat="server" style="margin-bottom: 0px" DataTextField="ctname" DataValueField="cityid">
+			</asp:DropDownList>
+		</p>
+		<p style="margin-left: 240px">
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </p>
+		<p style="margin-left: 240px">
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			</p>
+		<p style="margin-left: 360px">
+			&nbsp;</p>
+		<p>
+			<br />
+		</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			&nbsp;</p>
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DropDown1.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DropDown
+{
+	public partial class DropDown1 : System.Web.UI.Page
+	{
+		SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Conns"].ConnectionString);
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!IsPostBack)
+			{
+				Fill_Country();
+				DDState.Items.Insert(0, new ListItem("--SELECT STATE--", "0"));
+				DDCity.Items.Insert(0, new ListItem("--SELECT CITY--", "0"));
+
+				DDState.Enabled = true;
+				DDCity.Enabled = true;
+			}
+		}
+		public void Fill_Country()
+		{
+			DataSet ds = new DataSet();
+			ds = ShowData("Sp_Countries", null);
+			if (ds.Tables[0].Rows.Count > 0)
+			{
+				ddCountry.DataSource = ds;
+				ddCountry.DataBind();
+				ddCountry.Items.Insert(0, new ListItem("--SELECT COUNTRY--", "0"));
+			}
+		}
+		private DataSet ShowData(string SP_Name, SqlParameter sp)
+		{
+			SqlDataAdapter da = null;
+			DataSet ds = null;
+			using (da = new SqlDataAdapter(SP_Name, con))
+			{
+				da.SelectCommand.CommandType = CommandType.StoredProcedure;
+				if (sp != null)
+				{
+					da.SelectCommand.Parameters.Add(sp);
+				}
+				using (ds = new DataSet())
+				{
+					da.Fill(ds);
+					return ds;
+				}
+			}
+		}
+		protected void ddCountry_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			try
+			{
+				SqlParameter sp = null;
+				DataSet ds = null;
+				if (ddCountry.SelectedValue == "0")
+				{
+					DDState.SelectedValue = "0";
+					DDState.Enabled = false;
+				}
+				else
+				{
+					DDState.Enabled = true;
+					sp = new SqlParameter("@cid", ddCountry.SelectedValue);
+					ds = new DataSet();
+					ds = ShowData("Sp_States", sp);
+					if (ds.Tables[0].Rows.Count > 0)
+					{
+						DDState.DataSource = ds;
+						DDState.DataBind();
+						DDState.Items.Insert(0, new ListItem("--SELECT STATE--", "0"));
+					}
+				}
+				DDCity.SelectedValue = "0";
+				DDCity.Enabled = false;
+			}
+			catch(Exception ex)
+			{
+				Response.Write(ex.Message);
+			}
+		}
+
+		protected void DDState_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			try
+			{
+				SqlParameter sp=null;
+				DataSet ds=null;
+				if (DDState.SelectedValue == "0")
+				{
+					DDCity.SelectedValue = "0";
+					DDCity.Enabled = false;
+				}
+				else
+				{
+					DDCity.Enabled = true;
+					sp = new SqlParameter("@sid", DDState.SelectedValue);
+					ds = new DataSet();
+					ds = ShowData("Sp_Cities", sp);
+					if (ds.Tables[0].Rows.Count > 0)
+					{
+						DDCity.DataSource = ds;
+						DDCity.DataBind();
+						DDCity.Items.Insert(0, new ListItem("--SELECT CITY--", "0"));
+					}
+				}
+			}
+			catch(Exception ex)
+			{
+				Response.Write(ex.Message);
+			}
+		}
+
+		protected void btnSave_Click(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
